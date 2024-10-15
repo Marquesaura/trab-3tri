7e2031e5f458d6f79dc8b2b170b1d375e784b168
Laura Marques <161334577+Marquesaura@users.noreply.github.com>
2024-10-15 09:10:58 -0300

    Create myScript.js

---
+++ myScript.js
@@ -0,0 +1,25 @@
+function calcularImc() {
+    var altura = document.getElementById("altura").value;
+    var peso = document.getElementById("peso").value;
+
+    console.log(altura);
+    console.log(peso);
+
+    var imc = peso / (altura) ** 2;
+
+    if (imc < 18.5) {
+        window.alert("IMC está abaixo do peso!");
+    }
+    else if (imc < 24.9) {
+        window.alert("IMC está no peso ideal!");
+    }
+    else if (imc < 29.9) {
+        window.alert("IMC está sobrepeso!");
+    }
+    else if (imc < 39.9) {
+        window.alert("IMC está Obesidade tipo I!");
+    }
+    else if (imc > 39.9) {
+        window.alert("IMC está Obesidade tipo II!");
+    }
+}
